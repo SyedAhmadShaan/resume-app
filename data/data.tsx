83ccf1b58164ede43261e5b7266bcbe3072e1f85
SyedAhmadShaan <SyedAhmadShaan@users.noreply.github.com>
2024-08-07 01:08:59 +0500

    adding data

---
+++ data/data.tsx
@@ -0,0 +1,64 @@
+export const personalData = {
+	name: "Syed Ahmad Shaan",
+	role: "Frontend Developer",
+	education: [
+        "Cloud Native & Applied Generative AI Engineering | PIAIC | Year-2024",
+        "BS in Business Adminstration (Finance) | Virtual University of Pakistan | Year-2020",
+	],
+
+	contactLinks: [
+		"mailto:emailto:ahmadthegreat91@gmail.com",
+        "https://www.linkedin.com/in/syed-ahmad-shan/",
+		"https://github.com/SyedAhmadShaan",
+		"https://wa.me/+923404441223",
+	],
+};
+
+export const aboutMe = {
+	title: "About Me",
+	body: [
+		"I am a motivated and driven individual transitioning from a background in Business Administration (Finance) to the exciting world of technology. Currently, I am pursuing a program in Cloud Native & Applied Generative AI Engineering, where I am deepening my skills in cutting-edge technologies like Next.js and TypeScript.",
+		"My unique blend of business acumen and technical expertise allows me to approach problems with a strategic mindset, making complex challenges more manageable. I am passionate about leveraging technology to create innovative solutions and make a positive impact.",
+        "In my free time, I enjoy playing games, watching movies, and spending time with friends and family. I believe that my determination, adaptability, and collaborative spirit make me a valuable asset to any team. I am eager to continue learning and contributing to projects that drive meaningful change and innovation.",
+	],
+};
+
+export const skills = {
+	soft: [
+		{ icon: "👂🏼", text: "Active Listening" },
+		{ icon: "💬", text: "Effective Communication" },
+		{ icon: "👥", text: "Collaboration" },
+		{ icon: "⽓", text: "Teamwork" },
+		{ icon: "💡", text: "Creative Problem Solving" },
+		{ icon: "⌛️", text: "Time management" },
+	],
+	hard: [
+        { icon: "ʦ", text: "TypeScript" },
+		{ icon: "💻", text: "NextJs" },
+		{ icon: "🚀", text: "React" },
+		{ icon: "💾", text: "SQL" },
+		{ icon: "💾", text: "noSQL" },
+		{ icon: "📈", text: "Data Analytics" },
+
+	],
+};
+
+export const professionalData = {
+	title: "Professional Experience",
+	experiences: [
+		{
+			role: "Data Analyst, Upwork. (2023-Present)",
+			description:
+			"I am currently working as a Data Analyst on Upwork (2023-Present), where I provide data-driven solutions to a diverse range of clients. My responsibilities include gathering and preprocessing large datasets, performing detailed analyses using statistical tools, and identifying trends and insights to guide business decisions. I create comprehensive reports and dynamic dashboards using Excel, Tableau, and Power BI to present data clearly and effectively. I work closely with clients to understand their needs and develop tailored solutions, ensuring that my analyses support their strategic goals. This role has enhanced my technical skills and strengthened my ability to communicate complex information clearly.",	
+            current: true,
+		},
+
+		{
+			role: "Data Entry Expert, Fiverr. (2017-Present)",
+			description:
+			"Since 2017, I have also been a Data Entry Expert on Fiverr, offering accurate and efficient data entry services to clients worldwide. I manage large volumes of data, ensuring integrity and consistency across various platforms. I collaborate with clients to meet their specific data entry requirements, delivering high-quality results within tight deadlines. This experience has honed my attention to detail and problem-solving skills, enabling me to work independently and efficiently. I consistently deliver results that exceed client expectations, contributing to their operational success.",
+            current: false,
+		},
+
+	],
+};
