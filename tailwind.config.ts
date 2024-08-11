7b3fa0978287b61cd495e10b5e1443b5cfc82e1e
SyedAhmadShaan <SyedAhmadShaan@users.noreply.github.com>
2024-08-12 01:10:49 +0500

    configure tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,49 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      black: "#191A19",
+      yellow: "#FFF734",
+      green: "#68F590",
+      grey: "#F3F3F3",
+      "grey-light": "#FAFAFA",
+      "green-light": "#d2eff1",
+      white: "#fff",
+      teal: "#5eead4",
+    },
+    fontFamily: {
+      sans: ["Georgia", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      display: ["Oswald"],
+    },
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
       },
     },
   },
